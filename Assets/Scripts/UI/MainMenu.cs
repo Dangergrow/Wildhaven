ad2311a302f1f751072aac649f2de195325dcc71
unknown <Dangergrow@gmail.com>
2026-06-21 17:09:47 +0300

    MainMenu now uses CharacterCreator flow, skills mapping fixed

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -80,15 +80,71 @@ public class MainMenu : MonoBehaviour
 
     void OnNewGame()
     {
-        // Delete save so fresh world generates
         var savePath = System.IO.Path.Combine(Application.persistentDataPath, "world.sav");
         if (System.IO.File.Exists(savePath)) System.IO.File.Delete(savePath);
-        StartGame();
+
+        // Show character creator, then start game
+        var cc = gameObject.AddComponent<CharacterCreator>();
+        cc.OnComplete = (templates) =>
+        {
+            var spawner = FindObjectOfType<ColonistSpawner>();
+            if (spawner != null)
+            {
+                // Clear existing colonists
+                foreach (var c in spawner.Colonists.ToArray())
+                    Destroy(c.gameObject);
+                spawner.Colonists.Clear();
+
+                // Spawn from templates
+                foreach (var t in templates)
+                {
+                    Vector3 pos = FindSpawnPos();
+                    var go = Instantiate(spawner.colonistPrefab, pos, Quaternion.identity);
+                    var col = go.GetComponent<Colonist>();
+                    if (col == null) continue;
+                    col.colonistName = t.colonistName;
+                    col.age = t.age; col.isMale = t.isMale;
+                    for (int i = 0; i < 14; i++) SetSkill(col, i, t.skills[i]);
+                    col.perk = t.perk; col.flaw = t.flaw;
+                    spawner.Colonists.Add(col);
+                }
+            }
+            StartGame();
+        };
+        _canvas.gameObject.SetActive(false);
+    }
+
+    Vector3 FindSpawnPos()
+    {
+        var gm = FindObjectOfType<GridManager>();
+        if (gm == null) return new Vector3(50, 15, 50);
+        for (int x = 45; x < 55; x++)
+        for (int z = 45; z < 55; z++)
+        for (int y = gm.Height - 1; y > 5; y--)
+        {
+            if (gm.GetBlock(x, y, z) == BlockType.Air && gm.GetBlock(x, y - 1, z) == BlockType.Grass)
+                return gm.GridToWorld(x, y, z);
+        }
+        return new Vector3(50, 15, 50);
     }
 
     void OnContinue()
     {
         StartGame();
+    }
+
+    void SetSkill(Colonist c, int idx, int val)
+    {
+        switch (idx)
+        {
+            case 0: c.constructionSkill = val; break; case 1: c.miningSkill = val; break;
+            case 2: c.cookingSkill = val; break; case 3: c.intellectualSkill = val; break;
+            case 4: c.medicineSkill = val; break; case 5: c.meleeSkill = val; break;
+            case 6: c.rangedSkill = val; break; case 7: c.craftingSkill = val; break;
+            case 8: c.farmingSkill = val; break; case 9: c.socialSkill = val; break;
+            case 10: c.animalHandlingSkill = val; break; case 11: c.huntingSkill = val; break;
+            case 12: c.tradingSkill = val; break; case 13: c.artisticSkill = val; break;
+        }
     }
 
     void StartGame()
